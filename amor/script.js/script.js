5204b359bd8f72ba0345f4236f55192e02238d2c
Diosefy Gonçalves <diosefy.dev@gmail.com>
2026-09-08 10:41:24 -0300

    Update script.js

--- amor/script.js/script.js
+++ amor/script.js/script.js
@@ -65,7 +65,7 @@ function handleSwipe() {
 }
 
 /* ===== CONTADOR ===== */
-const startDate = new Date(2026, 8, 8, 0, 0, 0);
+const startDate = new Date(2026, 7, 8, 0, 0, 0);
 
 function updateCounter() {
   const now = new Date();
